0e9ccac7af13b03d2c1fba71d8a17480ac11dd55
Eric Jeker <e.jeker@arcanys.com>
2024-04-24 07:40:47 +0800

    Clean-up The Thing Namer

--- 4. The Thing Namer 3000/Program.cs
+++ 4. The Thing Namer 3000/Program.cs
@@ -1,7 +1,5 @@
 ﻿Console.WriteLine("What kind of thing are we talking about?");
-var a = Console.ReadLine();
+var kind = Console.ReadLine();
 Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-var b = Console.ReadLine();
-var c = "Doom";
-var d = "3000";
-Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+var description = Console.ReadLine();
+Console.WriteLine("The " + description + " " + kind + " of Doom 3000!");
