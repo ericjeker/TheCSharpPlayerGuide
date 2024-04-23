b4ad475d42432fd352dcbbe05d2b453a75cfbb2c
Eric Jeker <e.jeker@arcanys.com>
2024-04-24 07:29:59 +0800

    Initial commit

---
+++ 4. The Thing Namer 3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+var a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+var b = Console.ReadLine();
+var c = "Doom";
+var d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
