b45926a7ffe20bec0dca6a9da399f45a2c6b8fb3
Eric Jeker <eric@wowww.ch>
2025-01-27 23:46:59 +0800

    feat: add scratches, level 12

---
+++ 12. The Replicator of DTo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = new int[5];
+int[] numbers2 = new int[5];
+
+void WriteArray<T>(T[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + " ");
+    }
+}
+
+Console.WriteLine("Please enter 5 numbers: ");
+
+for (int i = 0; i < 5; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers2[i] = numbers[i];
+}
+
+Console.Write("\nFirst array: ");
+WriteArray(numbers);
+
+Console.Write("\nSecond array: ");
+WriteArray(numbers2);
---
+++ Scratches/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int[] scores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int smallestScore = int.MaxValue;
+
+for (int i = 0; i < scores.Length; i++)
+{
+    int score = scores[i];
+    if (score < smallestScore)
+    {
+        smallestScore = score;
+    }
+}
+
+Console.WriteLine("The smallest score is {0}", smallestScore);
